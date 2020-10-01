963f78b0ca1a99b88474a39d0390425b4c6bc95c
Vladislav Vasilev <vladimirvlad-68@mail.ru>
2020-10-01 19:04:49 +0300

    +

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -9,10 +11,54 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const writableStream = fs.createWriteStream(`new.${filepath}`, {flags: 'wx'});
+
+  const limitStream = new LimitSizeStream({limit: 1000000});
+
+  let dataFile = [];
+
   switch (req.method) {
     case 'POST':
+        console.log(1, pathname);
+        if (pathname.indexOf('/') !== -1) {
+            console.log(2);
+            res.statusCode = 400;
+            res.end('Nested path');
+            return;
+        }
 
-      break;
+        req.on('end', function() {
+            console.log(4);
+            limitStream.on('error', function(){
+                console.log(5);
+                res.statusCode = 413;
+                res.end('file size limit exceeded');
+            });
+
+            writableStream.on('error', function(err) {
+                console.log(6);
+                if (err.code === 'EEXIST') {
+                    console.log(7);
+                    res.statusCode = 409;
+                    res.end('file has already existed');
+                    return;
+                }
+                fs.unlink(`new.${filepath}`);
+                res.statusCode = 500;
+                res.end();
+            });
+
+            writableStream.on('close', () => {
+                console.log(7);
+                res.statusCode = 201;
+                res.end('alright');
+            });
+
+            limitStream.pipe(writableStream);
+            limitStream.end();
+        });
+
+        break;
 
     default:
       res.statusCode = 501;
