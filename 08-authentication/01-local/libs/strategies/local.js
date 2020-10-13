677ebfee8e5777b5855a2091012149a7efa3014d
Vladislav Vasilev <vladimirvlad-68@mail.ru>
2020-10-13 21:49:50 +0300

    + 08 01

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,25 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
+    async function(email, password, done) {
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return done(null, false, 'Нет такого пользователя');
+        }
+
+        const correctPass = await user.checkPassword(password);
+
+        if (!correctPass) {
+            return done(null, false, 'Неверный пароль');
+        }
+
+        return done(null, {
+            email: user.email,
+            displayName: user.displayName,
+        });
+    }
 );
