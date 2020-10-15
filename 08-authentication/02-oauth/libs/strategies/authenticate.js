c3b484c60181615e0add9527b20bff0e850f5f6e
Vladislav Vasilev <vladimirvlad-68@mail.ru>
2020-10-15 19:10:34 +0300

    + fix

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,11 +1,11 @@
 const User = require('../../models/User');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
-    if (!email) {
-        return done(null, false, 'Укажите email');
-    }
 
     try {
+        if (!email) {
+            return done(null, false, 'Укажите email');
+        }
         let user = await User.findOne({email});
         if (!user) {
             user = await User.create({email, displayName});
